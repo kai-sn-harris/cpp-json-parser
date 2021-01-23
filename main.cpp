5c786ab79882ab327cbd4e9a76fe10262063f127
Kai <m3sv.is.me@gmail.com>
2021-01-23 19:35:40 +1100

    demo for lexer update

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-    Lexer lexer("[]{},\"asd\":");
+    Lexer lexer("[]{},\"asd\": true false 5 6.7 3.1415 ");
     Token tok = lexer.getNextToken();
     while(!lexer.charIsNone) {
         std::cout << tok.value << std::endl;
