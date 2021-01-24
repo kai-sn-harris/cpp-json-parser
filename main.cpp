63656d66ca5b2d3d2cfe31c54a33700e60c40765
Kai <m3sv.is.me@gmail.com>
2021-01-24 19:13:22 +1100

    Outputs the json text and if no errors are displayed, it is valid json

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include "lexer.h"
 #include "types.h"
+#include "parser.h"
 #include <iostream>
+#include <fstream>
 
-int main() {
-    Lexer lexer("[]{},\"asd\": true false 5 6.7 3.1415 null ");
-    Token tok = lexer.getNextToken();
-    while(!lexer.charIsNone) {
-        std::cout << tok.value << std::endl;
-        tok = lexer.getNextToken();
+int main(int argc, char* argv[]) {
+    std::string text, temp;
+    if(!argv[1]) {
+        std::cout << "Example usage: main.exe FILENAME" << std::endl;
+        exit(1);
     }
+
+    std::ifstream file(argv[1]);
+    if(!file.is_open()) {
+        std::cout << "Problem opening \"" << argv[1] << "\"" << std::endl;
+        exit(-1);
+    }
+    while(std::getline(file, temp))
+        text += temp + "\n";
+    file.close();
+
+    std::cout << text << std::endl;
+    
+    Lexer lexer(text);
+    Parser parser(lexer);
+    AST tree = parser.ast();
 }
