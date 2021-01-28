abc1c0c5ed2c9dc87cda3c9b42303b1b9cdcb331
Kai <m3sv.is.me@gmail.com>
2021-01-28 20:00:23 +1100

    updated main.cpp to work with new a.json

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char* argv[]) {
     JSON json("example json/a.json");
     json.generate();
-    std::cout << std::any_cast<std::string>(json.variables["string"]) << std::endl;
-    std::cout << std::any_cast<float>(json.variables["num"]) << std::endl;
+    auto array = std::any_cast<std::vector<std::any>>(json.variables["array"]);
+    auto string = std::any_cast<std::string>(array[0]);
+    auto objInArray = std::any_cast<std::map<std::string, std::any>>(array[1]);
+    auto stringInObjInArray = std::any_cast<std::string>(objInArray["objInArray"]);
+    std::cout << string << std::endl;
+    std::cout << stringInObjInArray << std::endl;
 }
