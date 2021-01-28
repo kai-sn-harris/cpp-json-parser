186643de8656d76860c77e2170a64f190e9040e6
Kai <m3sv.is.me@gmail.com>
2021-01-28 13:21:12 +1100

    Example that implements updates

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
-#include "lexer.h"
-#include "types.h"
-#include "parser.h"
 #include <iostream>
-#include <fstream>
+#include "JSON.h"
 
 int main(int argc, char* argv[]) {
-    std::string text, temp;
-    if(!argv[1]) {
-        std::cout << "Example usage: main.exe FILENAME" << std::endl;
-        exit(1);
-    }
-
-    std::ifstream file(argv[1]);
-    if(!file.is_open()) {
-        std::cout << "Problem opening \"" << argv[1] << "\"" << std::endl;
-        exit(-1);
-    }
-    while(std::getline(file, temp))
-        text += temp + "\n";
-    file.close();
-
-    std::cout << text << std::endl;
-    
-    Lexer lexer(text);
-    Parser parser(lexer);
-    AST tree = parser.ast();
+    JSON json("a.json");
+    json.generate();
+    std::cout << std::any_cast<std::string>(json.variables["string"]) << std::endl;
+    std::cout << std::any_cast<float>(json.variables["num"]) << std::endl;
+    // std::cout << json.get("string") << std::endl;
 }
