9a16749627db64d8e95fe2da3ee390fe6c1cf309
Kai <m3sv.is.me@gmail.com>
2021-01-29 10:56:46 +1100

    example usage with examples\ json/a.json

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@
 int main(int argc, char* argv[]) {
     JSON json("example json/a.json");
     json.generate();
-    auto array = std::any_cast<std::vector<std::any>>(json.variables["array"]);
-    auto string = std::any_cast<std::string>(array[0]);
-    auto objInArray = std::any_cast<std::map<std::string, std::any>>(array[1]);
-    auto stringInObjInArray = std::any_cast<std::string>(objInArray["objInArray"]);
-    std::cout << string << std::endl;
-    std::cout << stringInObjInArray << std::endl;
+    std::cout << json.get<std::string>("string") << std::endl;
+    std::cout << json.get<std::string>("obj.stringInObj") << std::endl;
+    std::cout << json.get<float>("number") << std::endl;
+    auto array = json.get<std::vector<std::any>>("array");
+    std::cout << std::any_cast<std::string>(array[1]);
 }
