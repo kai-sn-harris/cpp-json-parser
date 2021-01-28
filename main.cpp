de37c49c0a54d45f66db582d1e5a190ca7cd001a
Kai <m3sv.is.me@gmail.com>
2021-01-28 13:24:22 +1100

    looks for json in the folder

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 #include "JSON.h"
 
 int main(int argc, char* argv[]) {
-    JSON json("a.json");
+    JSON json("example json/a.json");
     json.generate();
     std::cout << std::any_cast<std::string>(json.variables["string"]) << std::endl;
     std::cout << std::any_cast<float>(json.variables["num"]) << std::endl;
-    // std::cout << json.get("string") << std::endl;
 }
