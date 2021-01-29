b079e55ebca22e23d40af54aec1d50328a4f154a
Kai <m3sv.is.me@gmail.com>
2021-01-29 11:33:51 +1100

    updated example to work with JSON::get<>() change

--- examples/a.cpp
+++ examples/a.cpp
@@ -4,9 +4,9 @@
 int main(int argc, char* argv[]) {
     JSON json("../example json/a.json");
     json.generate();
-    std::cout << json.get<std::string>("string") << std::endl;
-    std::cout << json.get<std::string>("obj.stringInObj") << std::endl;
-    std::cout << json.get<float>("number") << std::endl;
-    auto array = json.get<std::vector<std::any>>("array");
+    std::cout << json.get<std::string>("string", json.variables) << std::endl;
+    std::cout << json.get<std::string>("obj.stringInObj", json.variables) << std::endl;
+    std::cout << json.get<float>("number", json.variables) << std::endl;
+    auto array = json.get<std::vector<std::any>>("array", json.variables);
     std::cout << std::any_cast<std::string>(array[1]);
 }
