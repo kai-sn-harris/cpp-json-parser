0f79cb31083a1ec627ac43a7deec263cc5dd18b8
Kai <m3sv.is.me@gmail.com>
2021-01-30 19:51:54 +1100

    json starts with array example

---
+++ examples/c.cpp
@@ -0,0 +1,9 @@
+#include "../JSON.h"
+#include <iostream>
+
+int main() {
+    JSON json("../example json/c.json");
+    json.generate();
+    std::cout << json.get<std::string>("(ar)0.0.objectInArrayInRootArray") << std::endl;
+    std::cout << json.get<float>("(ar)1.object in root array") << std::endl;
+}
