94cdd36dd52081dd21993350b21d8cd25e2d147f
Kai <m3sv.is.me@gmail.com>
2021-01-30 20:08:26 +1100

    deleted comment

--- examples/c.cpp
+++ examples/c.cpp
@@ -3,7 +3,6 @@
 
 int main() {
     JSON json("../example json/c.json");
-    // json.generate();
     std::cout << json.get<std::string>("(ar)0.0.objectInArrayInRootArray") << std::endl;
     std::cout << json.get<float>("(ar)1.object in root array") << std::endl;
 }
