b2858e638d6f6744818c39badf9de361516c5e8d
Kai <m3sv.is.me@gmail.com>
2021-01-30 17:52:41 +1100

    updated examples

--- examples/a.cpp
+++ examples/a.cpp
@@ -4,10 +4,10 @@
 int main(int argc, char* argv[]) {
     JSON json("../example json/a.json");
     json.generate();
-    std::cout << json.get<std::string>("string", json.variables) << std::endl;
-    std::cout << json.get<std::string>("obj.stringInObj", json.variables) << std::endl;
-    std::cout << json.get<float>("number", json.variables) << std::endl;
-    std::cout << json.get<float>("array.0.how", json.variables) << std::endl;
-    std::cout << json.get<std::string>("array.2.0", json.variables) << std::endl;
-    std::cout << json.get<std::string>("array.2.1.0", json.variables) << std::endl;
+    std::cout << json.get<std::string>("string") << std::endl;
+    std::cout << json.get<std::string>("obj.stringInObj") << std::endl;
+    std::cout << json.get<float>("number") << std::endl;
+    std::cout << json.get<float>("array.0.how") << std::endl;
+    std::cout << json.get<std::string>("array.2.0") << std::endl;
+    std::cout << json.get<std::string>("array.2.1.0") << std::endl;
 }
