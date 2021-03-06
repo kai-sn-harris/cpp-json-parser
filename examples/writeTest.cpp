0abf64f05959dbbd832ff666429adb5ca1f80475
Kai <m3sv.is.me@gmail.com>
2021-03-06 19:38:37 +1100

    Using emojis and special characters works when writing to files

--- examples/writeTest.cpp
+++ examples/writeTest.cpp
@@ -3,6 +3,6 @@
 int main() {
     JSON json("example json/write.json");
     std::cout << json.get<std::string>("varName") << std::endl;
-    json.write("varName", "Goodbye");
+    json.write("varName", "Goodbye! 😂 ππππππππ");
     std::cout << json.get<std::string>("varName") << std::endl;
 }
