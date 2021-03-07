a5af53152030f239da3e3621623fbde781562379
Kai <m3sv.is.me@gmail.com>
2021-03-07 12:29:06 +1100

    example writing

--- examples/writeTest.cpp
+++ examples/writeTest.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     JSON json("example json/write.json");
-    std::cout << json.get<std::string>("varName") << std::endl;
-    json.write("varName", "Goodbye! 😂 ππππππππ");
-    std::cout << json.get<std::string>("varName") << std::endl;
+    std::cout << json.get<std::string>("obj.asd") << std::endl;
+    json.write("obj.asd", "goodbye");
+    std::cout << json.get<std::string>("obj.asd") << std::endl;
 }
