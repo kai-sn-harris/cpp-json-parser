7b542e6af4ad65623324adb7ab417b9541aa1d2f
Kai <m3sv.is.me@gmail.com>
2021-04-09 14:33:50 +1000

    showcasing new bug fixes in master 9d042a3

--- examples/writeTest.cpp
+++ examples/writeTest.cpp
@@ -2,7 +2,15 @@
 
 int main() {
     JSON json("example json/write.json");
-    std::cout << json.get<std::string>("obj.asd") << std::endl;
-    json.write("obj.asd", "goodbye");
-    std::cout << json.get<std::string>("obj.asd") << std::endl;
+    std::cout << "obj.asd : " << json.get<std::string>("obj.asd") << std::endl;
+    json.write("obj.asd", 5.0F);
+    std::cout << "obj.asd : " << json.get<float>("obj.asd") << std::endl;
+    json.write("obj.asd", true);
+    std::cout << "obj.asd : " << (json.get<bool>("obj.asd") ? "true" : "false") << std::endl;
+    json.write("obj.asd", "I've changed types a lot!");
+    std::cout << "obj.asd : " << json.get<std::string>("obj.asd") << std::endl;
+    
+    std::cout << "array.0 : " << json.get<std::string>("array.0") << std::endl;
+    json.write("array.0", 69.0F, false);
+    std::cout << "array.0 : " << json.get<float>("array.0") << std::endl;
 }
