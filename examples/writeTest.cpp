930f58bfc38f61940d74c04fc8c173229aa76f45
Kai <m3sv.is.me@gmail.com>
2021-04-09 19:57:16 +1000

    example of writing value NULL

--- examples/writeTest.cpp
+++ examples/writeTest.cpp
@@ -7,10 +7,10 @@ int main() {
     std::cout << "obj.asd : " << json.get<float>("obj.asd") << std::endl;
     json.write("obj.asd", true);
     std::cout << "obj.asd : " << (json.get<bool>("obj.asd") ? "true" : "false") << std::endl;
-    json.write("obj.asd", "I've changed types a lot!");
+    json.write("obj.asd", NULL);
     std::cout << "obj.asd : " << json.get<std::string>("obj.asd") << std::endl;
     
     std::cout << "array.0 : " << json.get<std::string>("array.0") << std::endl;
-    json.write("array.0", 69.0F, false);
+    json.write("array.0", 4.56F, false);
     std::cout << "array.0 : " << json.get<float>("array.0") << std::endl;
 }
